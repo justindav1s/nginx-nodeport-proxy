d037f7e194234bf1a9a26b2857b0b33b3c486b59
Justin Davis <justinndavis@gmail.com>
2021-11-15 14:23:35 +0000

    updates

---
+++ tools-image/build.sh
@@ -0,0 +1,29 @@
+#!/bin/bash
+
+export PROJECT=tcp-proxy
+export APP=custom-nginx
+
+
+oc project $PROJECT
+
+oc delete bc ${APP}
+oc delete secret quayio-dockercfg
+
+oc create secret docker-registry quayio-dockercfg \
+  --docker-server=${QUAYIO_HOST} \
+  --docker-username=${QUAYIO_USER} \
+  --docker-password=${QUAYIO_PASSWORD} \
+  --docker-email=${QUAYIO_EMAIL} \
+  -n $PROJECT
+
+oc new-app -f build-config.yaml \
+    -p BUILD_NAME=${APP} \
+    -p APPLICATION_LABEL=${APP} \
+    -p DOCKER_REGISTRY=quay.io \
+    -p DOCKER_REPOSITORY=justindav1s \
+    -p IMAGE_NAME=${APP} \
+    -p IMAGE_VERSION=latest \
+    -p GIT_URL=https://github.com/justindav1s/nginx-nodeport-proxy.git \
+    -n $PROJECT
+
+oc start-build -F ${APP} -n $PROJECT   
